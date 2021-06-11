8f4740d8a369b0a71f0ca3b1accb2ef749f38aa0
Mike Zrimsek <mikezrimsek@gmail.com>
2021-06-10 23:04:20 -0400

    add group

--- homeassistant/update_automations.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-
-sudo rm automations.yaml
-sudo wget https://raw.githubusercontent.com/mzrimsek/server-config/main/homeassistant/automations.yaml
---
+++ homeassistant/update_configs.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+sudo rm automations.yaml
+sudo wget https://raw.githubusercontent.com/mzrimsek/server-config/main/homeassistant/automations.yaml
+
+sudo rm groups.yaml
+sudo wget https://raw.githubusercontent.com/mzrimsek/server-config/main/homeassistant/groups.yaml
