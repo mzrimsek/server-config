24d7556486f952a0a78457a1d79ff5b6a2d31daa
Mike Zrimsek <mikezrimsek@gmail.com>
2021-10-29 00:05:08 -0400

    add themes to script

--- dashboard/update_config.sh
+++ dashboard/update_config.sh
@@ -6,4 +6,7 @@ sudo wget https://raw.githubusercontent.com/mzrimsek/server-config/main/dashboar
 sudo rm bookmarks.json
 sudo wget https://raw.githubusercontent.com/mzrimsek/server-config/main/dashboard/bookmarks.json
 
+sudo rm themes.json
+sudo wget https://raw.githubusercontent.com/mzrimsek/server-config/main/dashboard/themes.json
+
 sudo docker container restart dashboard
