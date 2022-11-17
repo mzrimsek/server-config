5a13d4936be6b676c708c0ccc059a61f5ef0f160
Mike Zrimsek <mikezrimsek@gmail.com>
2022-11-16 22:06:54 -0500

    update paths

--- services/dashboard/update_config.sh
+++ services/dashboard/update_config.sh
@@ -1,13 +1,13 @@
 #/bin/bash
 
 sudo rm apps.json
-sudo wget https://raw.githubusercontent.com/mzrimsek/server-config/main/dashboard/apps.json
+sudo wget https://raw.githubusercontent.com/mzrimsek/server-config/main/services/dashboard/apps.json
 
 sudo rm bookmarks.json
-sudo wget https://raw.githubusercontent.com/mzrimsek/server-config/main/dashboard/bookmarks.json
+sudo wget https://raw.githubusercontent.com/mzrimsek/server-config/main/services/dashboard/bookmarks.json
 
 sudo rm themes.json
-sudo wget https://raw.githubusercontent.com/mzrimsek/server-config/main/dashboard/themes.json
+sudo wget https://raw.githubusercontent.com/mzrimsek/server-config/main/services/dashboard/themes.json
 
 sudo rm greeter.json
 sudo wget https://raw.githubusercontent.com/phntxx/dashboard/master/data/greeter.json
