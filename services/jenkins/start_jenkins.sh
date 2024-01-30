ed5391866aaea3a6086f9a211f13ed093e76d8b2
Mike Zrimsek <mikezrimsek@gmail.com>
2024-01-29 21:38:46 -0500

    jenkins image working as expected again with updated configs and scripts

--- services/jenkins/start_jenkins.sh
+++ services/jenkins/start_jenkins.sh
@@ -3,4 +3,5 @@
 sudo docker container stop jenkins
 sudo docker container rm jenkins
 sudo docker pull jenkins/jenkins:lts
-sudo docker compose --env-file .env up --build -d
+# sudo docker compose --env-file .env up --build -d
+sudo docker compose up --build -d
